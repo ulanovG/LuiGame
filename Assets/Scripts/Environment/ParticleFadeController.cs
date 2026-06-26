44211cfedf6924b5b472dec43464f7888555fca4
Георгий Уланов <ulanov75@gmail.com>
2026-06-26 17:12:24 +0500

    lamps + pause ui

--- Assets/Scripts/Environment/ParticleFadeController.cs
+++ Assets/Scripts/Environment/ParticleFadeController.cs
@@ -1,14 +1,9 @@
-using System;
-using System.ComponentModel;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
+using Unity.Mathematics;
 
-[ExecuteInEditMode]
-public class HouseColorController : MonoBehaviour
+public class ParticleFadeController : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer sprite;
-    [SerializeField] private SpriteRenderer spriteShadow;
+    [SerializeField] private ParticleSystem particle;
     [SerializeField] private float maxDistance = 300f;
     [SerializeField] private float fadeIntencity = 1f;
     [SerializeField] private float fadePower = 2f;
@@ -17,20 +12,18 @@ public class HouseColorController : MonoBehaviour
 
     void Update()
     {
-        if (sprite != null)
+        if (particle != null)
         {
             var distance = math.abs(transform.position.z - Camera.main.transform.position.z) / maxDistance;
             var intencity = math.clamp(math.pow(distance * fadeIntencity, fadePower), 0, fadeMaximum);
             intencity = (math.round(intencity * cellShading)) / cellShading;
 
-            var newColor = sprite.color;
-            newColor.a = math.lerp(0, 1, intencity);
-            sprite.color = newColor;
-            if (spriteShadow != null)
-            {
-                spriteShadow.color = newColor;
-                spriteShadow.sortingOrder = (int)(Camera.main.transform.position.z - transform.position.z) - 500;
-            }
+            var main = particle.main;
+            var gradient = main.startColor;
+            var newColor = gradient.color;
+            newColor.a = math.lerp(1, 0, intencity);
+            gradient = newColor;
+            main.startColor = gradient;
         }
         else
         {
